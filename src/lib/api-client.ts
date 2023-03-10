45d160cc21e307f581a5b56aa7336dfad5b7721d
Mehdi Bounya <mehdi.bounya@gmail.com>
2023-03-10 19:12:32 +0100

    Improve ApiClient

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,35 +1,42 @@
-import axios, {AxiosError} from "axios";
-import {ApiHttpException} from "../exceptions/api-http.exception";
+import axios, { Axios, AxiosError } from 'axios';
+import { ApiHttpException } from '../exceptions/api-http.exception';
 
 export class ApiClient {
-    readonly API_BASE_URL = 'https://youcanpay.com/api';
+  readonly BASE_URL = 'https://youcanpay.com';
 
-    private getUrl(endpoint: string): string {
-        return `${this.API_BASE_URL}//${endpoint.replace(/^\//, '')}`
-    }
+  constructor(private readonly isSandboxMode: boolean, private readonly httpClient: Axios) {}
 
-    /**
-     * Endpoint without the base url
-     * e.g.: Pass /tokenize to POST to https://youcanpay.com/api/tokenize
-     * @param endpoint
-     * @param payload
-     */
-    async post(endpoint: string, payload: object) {
-        try {
-            const response = await axios.post(this.getUrl(endpoint), payload, {
-                headers: {
-                    "User-Agent": "skuid-ycp-sdk-ts",
-                }
-            });
-        } catch (e) {
-            if (e instanceof AxiosError) {
-                // Error is due to networking problem
-                if (e.cause) {
-                    throw e.cause;
-                }
-                // Error is due to invalid API response
-                throw new ApiHttpException(e.status ?? 0, e.response?.data ?? {});
-            }
+  private getUrl(endpoint: string): string {
+    const sandboxPrefix = this.isSandboxMode ? 'sandbox/' : '';
+    return `${this.BASE_URL}/${sandboxPrefix}api/${endpoint.replace(/^\//, '')}`;
+  }
+
+  /**
+   * Endpoint without the base url
+   * e.g.: Pass /tokenize to POST to https://youcanpay.com/api/tokenize
+   * @param endpoint
+   * @param payload
+   */
+  async post(endpoint: string, payload: object): Promise<any> {
+    try {
+      const response = await this.httpClient.post(this.getUrl(endpoint), payload);
+      return response.data;
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        // Error is due to networking problem
+        if (e.cause) {
+          throw e.cause;
         }
+        // Error is due to invalid API response
+        throw new ApiHttpException(e.response?.status ?? 0, e.response?.data ?? {});
+      }
+      // Unexpected error
+      throw e;
     }
+  }
+
+  paymentUrl(token: string): string {
+    const sandboxPrefix = this.isSandboxMode ? 'sandbox/' : '';
+    return `${this.BASE_URL}/${sandboxPrefix}payment-form/${token}`;
+  }
 }
---
+++ test/api-client.spec.ts
@@ -0,0 +1,92 @@
+import { ApiClient } from '../src/lib/api-client';
+import { Axios, AxiosError, AxiosResponse } from 'axios';
+import { ApiHttpException } from '../src/exceptions/api-http.exception';
+
+describe('ApiClient', () => {
+  let sandboxClient: ApiClient;
+  let prodClient: ApiClient;
+  let httpClient: Axios;
+
+  beforeEach(() => {
+    httpClient = {} as unknown as Axios;
+    sandboxClient = new ApiClient(true, httpClient);
+    prodClient = new ApiClient(false, httpClient);
+  });
+
+  describe('paymentUrl', () => {
+    it('should return correct url with sandbox mode on', () => {
+      expect(sandboxClient.paymentUrl('payment-token-here')).toEqual(
+        'https://youcanpay.com/sandbox/payment-form/payment-token-here',
+      );
+    });
+
+    it('should return correct url with sandbox mode off', () => {
+      expect(prodClient.paymentUrl('payment-token-here')).toEqual(
+        'https://youcanpay.com/payment-form/payment-token-here',
+      );
+    });
+  });
+
+  describe('post', () => {
+    it('should post to correct url with sandbox mode on', async () => {
+      httpClient.post = jest.fn().mockReturnValue({
+        data: {
+          key: 'value',
+        },
+      });
+      await sandboxClient.post('endpoint-here', {
+        data: 'value',
+      });
+      expect(httpClient.post).toBeCalledWith('https://youcanpay.com/sandbox/api/endpoint-here', {
+        data: 'value',
+      });
+    });
+
+    it('should post to correct url with sandbox mode off', async () => {
+      httpClient.post = jest.fn().mockReturnValue({
+        data: {
+          key: 'value',
+        },
+      });
+      await prodClient.post('endpoint-here', {
+        data: 'value',
+      });
+      expect(httpClient.post).toBeCalledWith('https://youcanpay.com/api/endpoint-here', {
+        data: 'value',
+      });
+    });
+
+    it('should return correct response data', async () => {
+      const resp = {
+        data: {
+          key: 'value',
+        },
+      };
+      httpClient.post = jest.fn().mockReturnValue(resp);
+      expect(await sandboxClient.post('end', {})).toEqual(resp.data);
+    });
+
+    it('should throw correct exception when networking problem', () => {
+      httpClient.post = jest.fn().mockImplementation(() => {
+        const error = new AxiosError();
+        error.cause = new Error('Thrown cause');
+        throw error;
+      });
+      expect(async () => await sandboxClient.post('test', {})).rejects.toThrow('Thrown cause');
+    });
+
+    it('should throw correct exception when http problem', () => {
+      httpClient.post = jest.fn().mockImplementation(() => {
+        const error = new AxiosError();
+        error.response = {
+          status: 500,
+          data: { error: 'Oops' },
+        } as unknown as AxiosResponse;
+        throw error;
+      });
+      expect(async () => await sandboxClient.post('test', {})).rejects.toThrow(
+        new ApiHttpException(500, { error: 'Oops' }),
+      );
+    });
+  });
+});
