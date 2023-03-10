13a107608d999f82d887c8f562651ae695fa789d
Mehdi Bounya <mehdi.bounya@gmail.com>
2023-03-10 19:16:38 +0100

    Create YcpFactory

---
+++ src/lib/ycp-factory.ts
@@ -0,0 +1,21 @@
+import { Youcanpay } from './youcanpay';
+import { ApiClient } from './api-client';
+import axios from 'axios';
+
+export class YcpFactory {
+  static ycp(config: YoucanpayConfig): Youcanpay {
+    let isSandboxMode: boolean | undefined = config.isSandboxMode;
+    if (isSandboxMode === undefined) {
+      isSandboxMode = process.env.NODE_ENV !== 'production';
+    }
+
+    const httpClient = axios.create({
+      headers: {
+        'User-Agent': 'skuid-ycp-sdk-ts',
+      },
+    });
+
+    const client = new ApiClient(isSandboxMode, httpClient);
+    return new Youcanpay(config.privateKey, client);
+  }
+}
