e634cb32bab920acc47e6109bed53f6bf8adc740
Mehdi Bounya <mehdi.bounya@gmail.com>
2023-03-10 17:56:11 +0100

    Create api-http.exception.ts

---
+++ src/exceptions/api-http.exception.ts
@@ -0,0 +1,8 @@
+/**
+ * Exception thrown when encountering an HTTP error with the API
+ */
+export class ApiHttpException extends Error {
+    constructor(public readonly statusCode: number, public readonly body: object) {
+        super();
+    }
+}
